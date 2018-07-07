c4897275f40b40e6d1503bfa5926bdb33c14fada
vitham <anhxtanhwp@gmail.com>
2018-07-07 20:40:05 +0700

    set current location

--- app/src/main/java/com/vit/trafficvolumemap/MapsActivity.java
+++ app/src/main/java/com/vit/trafficvolumemap/MapsActivity.java
@@ -1,5 +1,9 @@
 package com.vit.trafficvolumemap;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
@@ -11,7 +15,9 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity
-        implements OnMapReadyCallback{
+        implements OnMapReadyCallback {
+
+    private static final int MY_REQUEST_INT = 177;
 
     private GoogleMap mMap;
 
@@ -36,11 +42,28 @@ public class MapsActivity extends FragmentActivity
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION} , MY_REQUEST_INT);
+            }
+            return;
+        }
+        else {
+
+            mMap.setMyLocationEnabled(true);
+        }
+
+
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
-
-
 }
