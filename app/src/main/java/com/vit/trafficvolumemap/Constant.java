8105c3c08a8960a2acbc98e8677823b87756b9b6
vitham <anhxtanhwp@gmail.com>
2018-07-08 04:31:43 +0700

    add marker, circle position

---
+++ app/src/main/java/com/vit/trafficvolumemap/Constant.java
@@ -0,0 +1,5 @@
+package com.vit.trafficvolumemap;
+
+public interface Constant {
+    float LEVEL_ZOOM_DEFAULT = 15f;
+}
--- app/src/main/java/com/vit/trafficvolumemap/MapsActivity.java
+++ app/src/main/java/com/vit/trafficvolumemap/MapsActivity.java
@@ -2,52 +2,87 @@ package com.vit.trafficvolumemap;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity
-        implements OnMapReadyCallback {
+        implements OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
+
+    // ---------------------------------------------------------------------------------------------
+    // FIELDS
+    // ---------------------------------------------------------------------------------------------
     private static final int MY_REQUEST_INT = 177;
 
     private GoogleMap mMap;
 
+    private GoogleApiClient mGoogleApiClient;
 
+    private LatLng mCurrentLocation;
+
+    private LatLng mLatLngSearchPosition;
+
+
+    // ---------------------------------------------------------------------------------------------
+    // OVERRIDE METHODS
+    // ---------------------------------------------------------------------------------------------
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        mGoogleApiClient = new GoogleApiClient.Builder(this).addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+
         initView();
     }
 
-    private void initView() {
-        setContentView(R.layout.activity_maps);
-
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
     }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mGoogleApiClient.disconnect();
+    }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.getUiSettings().setCompassEnabled(true);
+        mMap.setTrafficEnabled(true);
+        mMap.setBuildingsEnabled(true);
 
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED
                 && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
 
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                 requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,
@@ -56,14 +91,120 @@ public class MapsActivity extends FragmentActivity
             return;
         }
         else {
-
             mMap.setMyLocationEnabled(true);
         }
 
+        onClickMap();
+    }
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if (ActivityCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            final Location lastLocation =
+                    LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+            if (lastLocation == null) {
+                return;
+            }
+            mCurrentLocation = new LatLng(lastLocation.getLatitude(), lastLocation.getLongitude());
+            if (mLatLngSearchPosition == null) {
+                showCameraToPosition(mCurrentLocation, Constant.LEVEL_ZOOM_DEFAULT);
+            }
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+
+
+    // ---------------------------------------------------------------------------------------------
+    // PRIVATE METHODS
+    // ---------------------------------------------------------------------------------------------
+
+    /**
+     * init view
+     */
+    private void initView() {
+        setContentView(R.layout.activity_maps);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    /**
+     * show marker when touch map
+     */
+    private void onClickMap() {
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                showMarkerToGoogleMap(latLng);
+                showCircleToGoogleMap(latLng, 0.5f);
+            }
+        });
+    }
+
+    /**
+     * show marker when user click a position on map
+     * @param position
+     */
+    public void showMarkerToGoogleMap(LatLng position) {
+        mMap.clear();
+        MarkerOptions markerOptions = new MarkerOptions().position(position);
+        markerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_location_active));
+        mMap.addMarker(markerOptions);
+    }
+
+
+    /**
+     * move camera to a position on map
+     * @param position
+     * @param zoomLevel
+     */
+    public void showCameraToPosition(LatLng position, float zoomLevel) {
+        CameraPosition cameraPosition = CameraPosition.builder()
+                .target(position)
+                .zoom(zoomLevel)
+                .bearing(0.0f)
+                .tilt(0.0f)
+                .build();
+
+        if (mMap != null) {
+            mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition), null);
+        }
+    }
+
+    /**
+     * draw circle on positon of map
+     * @param position
+     * @param radius
+     */
+    public void showCircleToGoogleMap(LatLng position, float radius) {
+        if (position == null) {
+            return;
+        }
+        CircleOptions circleOptions = new CircleOptions();
+        circleOptions.center(position);
+        //Radius in meters
+        circleOptions.radius(radius * 1000);
+        circleOptions.fillColor(getResources().getColor(R.color.circle_on_map));
+        circleOptions.strokeColor(getResources().getColor(R.color.circle_on_map));
+        circleOptions.strokeWidth(0);
+        if (mMap != null) {
+            mMap.addCircle(circleOptions);
+        }
     }
 }
